90079b038377838c6daef6145ec7cd03584f458a
Igniel <walthervallejo2018@gmial.com>
2024-12-03 14:03:24 +0100

    segunda version

---
+++ carpetaPrueba/otro.py
@@ -0,0 +1,17 @@
+'''otro archivo de pruebas'''
+import pygame
+
+
+# pylint: disable=no-member
+import pygame
+
+pygame.init()
+
+size = (800,500)
+screen = pygame.display.set_mode(size)
+
+while True:
+    pass
+
+
+
